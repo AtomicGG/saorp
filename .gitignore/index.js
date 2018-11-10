231c6926b7172b1b348f29fbefb7fbee50104600
AtomicGG <43680840+AtomicGG@users.noreply.github.com>
2018-11-10 13:02:30 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10532,5 +10532,49 @@ bot.on('message', message => {
 
 
 
+
+
+
+bot.on('message', message => {
+    let cont = message.content.slice(prefix.length).split(" ");
+    const args = cont.slice(1);
+  if (message.content.startsWith(prefix + "test récompenses")) {   
+        let Z = args.slice(2).join(" : ");
+    const B = (Math.floor((3*1)*Math.random()))
+    const BB = B*Z
+  const C = (Math.floor((3*1)*Math.random()))
+  const CC = C*Z
+  const D = (Math.floor((2-(0.75))*Math.random()))
+  const DD = D*Z
+  const E = (Math.floor((2-(0.85))*Math.random()))
+  const EE = E*Z
+  const F = (Math.floor((14*1)*Math.random()+14*1))
+  const FF = F*Z
+  const G = (Math.floor((11*1)*Math.random()+10*1))
+  const GG = G*Z
+  const I = (Math.floor((2-(0.90))*Math.random()))
+  const II = I*Z
+  const O = (Math.floor((2-(0.98))*Math.random()))
+  const OO = O*Z
+    const embed = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setAuthor(message.author.username , message.author.avatarURL)
+    .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
+    .setImage("https://vignette.wikia.nocookie.net/sao/images/0/02/Col.png/revision/latest?cb=20150705174105&path-prefix=es")
+    .addField("Récompenses :" , "\n:poultry_leg: Viande de loup : " +BB+ "\n:knife: Peau de loup : " +CC+ "\n:eye: Oeil de loup : " +DD+ "\n:cupid: Coeur de loup : " +EE+ "\n:gem: Croc de loup : " +II+ "\n:gem: Oeuf déformé : " +OO+ "\n:sparkles: Points d'expérience : " +FF+ ", aucun pour les niveaux 10 ou plus\n:large_orange_diamond: Cols : " +GG)
+    .setTimestamp()
+    message.channel.send({embed})
+        }
+  }) ;
+
+
+
+
+
+
+
+
+
+
       
 
