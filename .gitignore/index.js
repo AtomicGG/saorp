b6d826d86774cc9eea1c313724d8adb87497f74f
AtomicGG <43680840+AtomicGG@users.noreply.github.com>
2018-11-15 19:44:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5038,8 +5038,8 @@ bot.on('message', message => {
  let cont = message.content.slice(prefix.length).split(" ");
                   const args = cont.slice(1);
   if (message.content.startsWith(prefix + "Combattant")) {
-     if (talkedRecently.has(message.author.id+101)) {
-      const embed = new Discord.RichEmbed()
+     if (talkedRecently.has(message.author.id+1011)) {
+        const embed = new Discord.RichEmbed()
        .setAuthor(message.author.username , message.author.avatarURL)
        .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
        .setColor(3447003)
