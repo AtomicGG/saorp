4613c1c03fd3c8dbb48df496f25601dea3b4a7c0
Red <b.darras854@laposte.net>
2019-04-04 00:42:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13089,7 +13089,7 @@ bot.on('message', message => {
 
 bot.on('message', message => {
   const HP = (Math.floor((11)*Math.random()+10))
-  if (message.content.equals(prefix + "Achat/fabrication/découverte du ragoût de lapin")) {
+  if (message.content.includes(prefix + "Achat/fabrication/découverte du ragoût de lapin")) {
     const embed = new Discord.RichEmbed()
     .setAuthor(message.author.username , message.author.avatarURL)
     .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
