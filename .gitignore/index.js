5ce749c1d02c6b066e55683637b1729270c779e4
Red <b.darras854@laposte.net>
2019-05-04 22:18:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5932,8 +5932,7 @@ bot.on('message', message => {
       message.channel.send({embed})
     } else {
       const schema = Math.floor(5*Math.random()+1)
-      switch(schema){
-        case 1 :
+      if(schema == 1) {
         const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
@@ -5942,46 +5941,46 @@ bot.on('message', message => {
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
         message.channel.send({embed})
-        break
-        case 2 :
-        const bla = new Discord.RichEmbed()
+      }
+      if(schema == 2){
+        const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
         .setColor(3447003)
         .addField(":sunrise_over_mountains: Plaines cas 2 :" , ":sunrise_over_mountains: En marchant dans les plaines, vous rencontrez les ennemis suivants :\n\n:crossed_swords: Renard(s) : 2\n:crossed_swords: Sanglier(s) : 0\n:crossed_swords: Chien(s) : 0\n:crossed_swords: Bandit(s) débutant(s) : 0\n:crossed_swords: Lapin(s) géant(s) : 0")
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
-        message.channel.send({bla})
-        break
-        case 3 :/*
-        const embed3 = new Discord.RichEmbed()
+        message.channel.send({embed})
+      }
+      if(schema == 3){
+        const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
         .setColor(3447003)
         .addField(":sunrise_over_mountains: Plaines cas 3 :" , ":sunrise_over_mountains: En marchant dans les plaines, vous rencontrez les ennemis suivants :\n\n:crossed_swords: Renard(s) : 2\n:crossed_swords: Sanglier(s) : 1\n:crossed_swords: Chien(s) : 0\n:crossed_swords: Bandit(s) débutant(s) : 0\n:crossed_swords: Lapin(s) géant(s) : 0")
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
-        message.channel.send({embed3})*/
-        break
-        case 4 :/*
-        const embed4 = new Discord.RichEmbed()
+        message.channel.send({embed})
+      }
+      if(schema == 4){
+        const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
         .setColor(3447003)
         .addField(":sunrise_over_mountains: Plaines cas 4 :" , ":sunrise_over_mountains: En marchant dans les plaines, vous rencontrez les ennemis suivants :\n\n:crossed_swords: Renard(s) : 2\n:crossed_swords: Sanglier(s) : 1\n:crossed_swords: Chien(s) : 1\n:crossed_swords: Bandit(s) débutant(s) : 0\n:crossed_swords: Lapin(s) géant(s) : 0")
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
-        message.channel.send({embed4})*/
-        break
-        case 5 :/*
-        const embed5 = new Discord.RichEmbed()
+        message.channel.send({embed})
+      }
+      if(schema == 5){
+        const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
         .setColor(3447003)
         .addField(":sunrise_over_mountains: Plaines cas 5 :" , ":sunrise_over_mountains: En marchant dans les plaines, vous rencontrez les ennemis suivants :\n\n:crossed_swords: Renard(s) : 2\n:crossed_swords: Sanglier(s) : 0\n:crossed_swords: Chien(s) : 1\n:crossed_swords: Bandit(s) débutant(s) : 0\n:crossed_swords: Lapin(s) géant(s) : 0")
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
-        message.channel.send({embed5})*/
+        message.channel.send({embed})
       }
     }
     talkedRecently.add(message.author.id+1000);
