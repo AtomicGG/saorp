51b0417e9c15ae157d61ffc4d94029fb5c5c9e3f
Red <b.darras854@laposte.net>
2019-05-04 22:06:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5943,15 +5943,15 @@ bot.on('message', message => {
         .setTimestamp()
         message.channel.send({embed})
         break
-        case 2 :/*
-        const embed2 = new Discord.RichEmbed()
+        case 2 :/
+        const embed = new Discord.RichEmbed()
         .setAuthor(message.author.username , message.author.avatarURL)
         .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
         .setColor(3447003)
         .addField(":sunrise_over_mountains: Plaines cas 2 :" , ":sunrise_over_mountains: En marchant dans les plaines, vous rencontrez les ennemis suivants\n\n:crossed_swords: Renard(s) : 2\n:crossed_swords: Sanglier(s) : 0\n:crossed_swords: Chien(s) : 0\n:crossed_swords: Bandit(s) débutant(s) : 0\n:crossed_swords: Lapin(s) géant(s) : 0")
         .setImage("https://i.pinimg.com/originals/74/ef/20/74ef206acce786bab2081e1fae7aa94e.jpg")
         .setTimestamp()
-        message.channel.send({embed2})*/
+        message.channel.send({embed})
         break
         case 3 :/*
         const embed3 = new Discord.RichEmbed()
