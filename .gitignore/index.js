aef82e165b629b926b7f474a0eaa27b9316e586e
Red <b.darras854@laposte.net>
2019-05-29 22:02:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14597,7 +14597,7 @@ bot.on('message', message => {
 
 // Grotte | Monstres | Récompenses | MAJ
 
-/*bot.on('message', message => {
+bot.on('message', message => {
   let cont = message.content.slice(prefix.length).split(" ");
   const args = cont.slice(1);
   if (message.content.startsWith(prefix + "Kobolt keep récompenses solo")) {
@@ -14641,7 +14641,7 @@ bot.on('message', message => {
     }
   }
 }) ;
-
+/*
 bot.on('message', message => {
   let cont = message.content.slice(prefix.length).split(" ");
   const args = cont.slice(1);
