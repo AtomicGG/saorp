1da5ebeba35b27dd8c3867e6da0b02dd7371647c
Red <b.darras854@laposte.net>
2019-06-01 17:55:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26363,7 +26363,7 @@ bot.on('message', message => {
 });
 
 bot.on('message', message => {
-  if (message.content.startsWith(prefix + "Plan d'arme  kobolt")) {
+  if (message.content.startsWith(prefix + "Plan d'arme kobolt")) {
     const embed = new Discord.RichEmbed()
     .setAuthor(message.author.username , message.author.avatarURL)
     .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
