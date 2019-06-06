b596cac9ee24b21f40bac5b4fb0a32d470a7eb7e
Red <b.darras854@laposte.net>
2019-06-06 02:53:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6681,7 +6681,7 @@ bot.on('message', message => {
   let cont = message.content.slice(prefix.length).split(" ");
   const args = cont.slice(1);
   if (message.content.startsWith(prefix + "Blood insanity")) {
-    if (/*talkedRecently.has(message.author.id+5)*/false) {
+    if (talkedRecently.has(message.author.id+5)) {
       const embed = new Discord.RichEmbed()
       .setAuthor(message.author.username , message.author.avatarURL)
       .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
