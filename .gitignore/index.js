4b0530594b52df3db5b27c395ed204be882167db
Fenibax <43680840+AtomicGG@users.noreply.github.com>
2019-06-21 17:42:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -22818,7 +22818,7 @@ bot.on('message', message => {
 
 // Vallée | Combat
 
-bot.on('message', message => {
+bot.off('message', message => {
   let cont = message.content.slice(prefix.length).split(" ");
   const args = cont.slice(1);
   if (message.content.startsWith(prefix + "Vallée combat")) {
