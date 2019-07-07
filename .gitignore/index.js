3b4a075ce2dbbee72f048549508e380bfb32546d
Fenibax <43680840+AtomicGG@users.noreply.github.com>
2019-07-07 21:30:12 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13699,7 +13699,7 @@ bot.on('message', message => {
     .setAuthor(message.author.username , message.author.avatarURL)
     .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
     .setColor(3447003)
-    .addField(":scales: Découverte de l'écu du roi déchu à une main :" , ":scales: Vous venez de découvrir cet objet !\n\n:shield: Points d'armure : " +Défense+ "\n:gift_heart: HP : " +HP+ "\n\n:warning: Résistance au saignement : 40 et Il est impossible d'avoir un autre bouclier en le portant!")
+    .addField(":scales: Découverte de l'écu du roi déchu à une main :" , ":scales: Vous venez de découvrir cet objet !\n\n:shield: Points d'armure : " +Défense+ "\n:gift_heart: HP : " +HP+ "\n\n:crossed_swords: Ce bouclier offre l'attaque suivante :\n\n:crossed_swords: =Protection du roi déchu\n\n:warning:Il est impossible d'avoir un autre bouclier en le portant!")
     .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
     .setTimestamp()
     message.channel.send({embed})
