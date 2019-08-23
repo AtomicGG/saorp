7aefea4657d8131520d31ffc554442fcd21e87ed
Red <b.darras854@laposte.net>
2019-08-23 20:40:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -889,11 +889,10 @@ bot.on('message', message => {
       .setAuthor(message.author.username, message.author.avatarURL)
       .setFooter("『SAO Community [RP]』©", "http://www.copyrightfrance.com/images/copyright.png")
       .setColor(3447003)
-      .addField("Points de Vie :", pv)
-      .addBlankField(true)
-      .addField("Attaque : ", atk)
-      .addBlankField(true)
-      .addField("Défense : ", def)
+      .setTitle()
+      .addField("Les statistiques de base d'un combattant de niveau " + niveauInt + " :", "Points de vie : " + pv + "\n" +
+                                                                                          "Attaque : " + atk + "\n" + 
+                                                                                          "Défense : " + def)
       .setImage("https://i.pinimg.com/originals/18/fb/4b/18fb4b82ad92387d26413f1ef3518d96.png")
       .setTimestamp()
     message.channel.send({ embed })
